7f8a70e7f4345ba1a4203dc5d32b0410c2c0f57a
Krystel Antonella Lucin Asanza <klucin@espol.edu.ec>
2022-06-24 01:49:21 -0500

    reto

--- scripts/ejercicio.js
+++ scripts/ejercicio.js
@@ -1,28 +1,27 @@
 let ejecutarCodigo = () => {
-    //alert('Hola, mundo!');
-    let elemento1 = document.getElementById('titulo1')
-    elemento1.textContent = 'Album de fotos'
+    let elemento = document.getElementById("titulo1");
+    elemento.textContent = 'Album de fotos';
 
-    let arreglo = document.getElementsByClassName('text-muted')
-    let elemento2 = arreglo[1]
-    let htmlCode = '<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href="https://codepen.io/taj1uddin/pen/eYVrLKy">Codepen - Taj Uddin</a>.</span>'
-    elemento2.innerHTML = htmlCode
+    let arreglo = document.getElementsByClassName('text-muted');
+    elemento = arreglo[1];
+    elemento.innerHTML = '<span> En este sitio encontrarás un album de fotos inspirado en el snippet de <a href="https://codepen.io/taj1uddin/pen/eYVrLKy">Codepen - Taj Uddin</a>.</span>'
 
-    let arreglo1 = document.getElementsByTagName('p')
-    let elemento3 = arreglo1[2]
-    elemento3.setAttribute('class', 'd-none')
 
-    let svg = document.querySelectorAll('svg')
-    let htmlimg = [ 'url: "https://images.unsplash.com/photo-1653942786759-f3caff948222?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: "camino"', 
-            'url: "https://images.unsplash.com/photo-1653988235129-842891001e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: "energia"', 
-            'url: "https://images.unsplash.com/photo-1648737963540-306235c8170e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", alt: "papá"' ]
-    for (let i = 0; i < svg.length; i++) {
-        let img = document.createElement('img')
-        img.innerHTML = htmlimg[0]
-        svg[i].replaceWith(img)
-    }
-            
-    
+    let arreglo2 = document.getElementsByTagName('p');
+    elemento = arreglo2[2];
+    elemento.setAttribute('class','d-none');
+
+
+    let imagenes = [ { url: 'https://images.unsplash.com/photo-1653942786759-f3caff948222?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDl8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'camino'}, { url: 'https://images.unsplash.com/photo-1653988235129-842891001e10?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'energia'}, { url: 'https://images.unsplash.com/photo-1648737963540-306235c8170e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwzMDF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: 'papá'}, { url: 'https://images.unsplash.com/photo-1653936999234-6574484abc38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMTd8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: '1'}, { url: 'https://images.unsplash.com/photo-1653559260129-d4347e4c8df0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzMjJ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: '2'}, { url: 'https://images.unsplash.com/photo-1648737962619-16e5cd465d2b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxlZGl0b3JpYWwtZmVlZHwyOTF8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: '3'},{ url: 'https://images.unsplash.com/photo-1578255321055-d9ed6a976af5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMjZ8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: '4'}, { url: 'https://images.unsplash.com/photo-1654006018654-9f03ad2a02b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTR8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: '5'}, { url: 'https://images.unsplash.com/photo-1654002300319-6c882ad161ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOTB8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60', alt: '6'}]
+
+    for (let imagen of imagenes){
+
+        let div = document.querySelector('div > svg');
+        const span = document.createElement('img');
+        span.src = imagen.url;
+        span.alt = imagen.alt;
+        div.replaceWith(span);       
+    } 
 }
 
-ejecutarCodigo();
+ejecutarCodigo()
